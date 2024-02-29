ee1c7b3f89a1f21ca0d413c4f4f183bc52758783
Jose Carrillo <jacg182010@gmail.com>
2024-02-29 16:04:50 -0600

    Add dotenv-rails

--- app/models/videogame.rb
+++ app/models/videogame.rb
@@ -2,4 +2,5 @@ class Videogame < ApplicationRecord
   belongs_to :user
   validates :title, :rating, :condition, :price, presence: true
   validates :rating, inclusion: 0..5
+  has_one_attached :photo
 end
