0fc46abb6155189bedce1f3dc7a3516ffb9b7967
Jose Carrillo <jacg182010@gmail.com>
2024-03-04 14:10:23 -0600

    trabajando en la imagen de profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @user = User.find(current_user.id) if current_user.photo_user.key
   end
 end
