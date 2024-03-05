3e788a36d604e0501970d5d0ca1bcb457594104a
javier galviz <jegalviz2111@gmail.com>
2024-03-05 09:56:52 -0400

    Listo mapbox

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   def show_seller
     @videogame = Videogame.find(params[:videogame_id])
     @user = @videogame.user
+    @markers = [
+      {
+        lat: @user.latitude,
+        lng: @user.longitude
+      }
+    ]
   end
 
   def show
